c0c81da771e31c95207569a29a040ff2db7c9fb7
igregorio <igregoriodoneto@gmail.com>
2020-02-25 01:20:53 -0300

    Query e Route params

--- modulo01/index.js
+++ modulo01/index.js
@@ -1,10 +1,21 @@
 const express = require('express');
 
 const server = express();
+
+// Query params = ?nome=NodeJs
+// Route params = /curso/2
+// Request body = {nome: 'NodeJs', tipo: 'Backend'}
+
 // localhost:porta/curso
-server.get('/curso',(req,res)=>{
+server.get('/curso/:id',(req,res)=>{
+
+  // const nome = req.query.nome;
+  const id = req.params.id;
+
   // return res.send("Hello world!");
-  return res.json({curso: "Node js"});
+  // return res.json({curso: `Aprendendo ${nome}`});
+
+  return res.json({curso: `Curso: ${id}`});
 });
 
 server.listen(3000);
