63f053a3a99bf564b7acda9669785b88ad41810e
igregorio <igregoriodoneto@gmail.com>
2020-02-25 16:17:09 -0300

    Modificando pesquisa com index como parâmetro

--- modulo01/index.js
+++ modulo01/index.js
@@ -6,16 +6,20 @@ const server = express();
 // Route params = /curso/2
 // Request body = {nome: 'NodeJs', tipo: 'Backend'}
 
+var cursos = ['Node js', 'JavaScript', 'React Native'];
+
 // localhost:porta/curso
-server.get('/curso/:id',(req,res)=>{
+server.get('/curso/:index',(req,res)=>{
 
   // const nome = req.query.nome;
-  const id = req.params.id;
+  // const id = req.params.id;
+
+  const {index} = req.params;
 
   // return res.send("Hello world!");
   // return res.json({curso: `Aprendendo ${nome}`});
-
-  return res.json({curso: `Curso: ${id}`});
+  // return res.json({curso: `Curso: ${id}`});
+  return res.json(cursos[index]);
 });
 
 server.listen(3000);
